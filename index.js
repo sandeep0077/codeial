a30809e52bb25e17e1b0d0a6aa6a64e02cf5ec5e
sandeep0077 <76220371+sandeep0077@users.noreply.github.com>
2021-02-13 11:42:32 +0530

    install ejs and setup the view engine and defined path

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const port = 8000;
 // use express router
 app.use('/',require('./routes'));
 
+app.set('view engine','ejs');
+app.set('views','./views');
 
+// set up the view engine
 app.listen(port, function (err) {
     if (err) { 
         console.log(`Error in ruuning the server : ${err}`) 
