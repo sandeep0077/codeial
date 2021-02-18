fe45e4e2ed682de14df5a2f918cb7de8c525174c
sandeep0077 <76220371+sandeep0077@users.noreply.github.com>
2021-02-18 12:13:40 +0530

    Storing session permentely,settingup mongo store annd relevant library  for session cookies

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
 
+//Server is every time resetting the server cookie and telling the browser to remove it from itself,So store it i n your db/mongodb.
+// for that we use "mongo store" and a library called "connect mongo"
+//It requires an argument (session ) unlike any other library till now
+const MongoStore  = require('connect-mongo')(session);
 
 app.use(express.urlencoded());
 
@@ -31,6 +35,8 @@ app.set('layout extractScripts',true);
 app.set('view engine','ejs');
 app.set('views','./views');
 
+
+//MongoStore is used to store the session cookie in db
 app.use(session({
     name:'codeial',
     // TOdO change the secrete before deployment in production mode 
@@ -39,7 +45,16 @@ app.use(session({
     resave:false,
     cookie: {
         maxAge:(1000 * 60 * 100)
+    },
+    store:new MongoStore({
+        mongooseConnection:db,
+        autoRemove:'disabled'
+    },
+    //Call back function incase if the connection is not established
+    function(err){
+        consile.log(err || "connect-mongodb setup ok");
     }
+    )
 }));
 
 //tell the app to use passoport
