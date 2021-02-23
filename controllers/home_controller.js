fe42a037502059e53948bdc0b4cb99d7223fe201
sandeep0077 <76220371+sandeep0077@users.noreply.github.com>
2021-02-23 14:22:43 +0530

    Converting to async await

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -2,37 +2,39 @@
 const Post = require('../models/post');
 const User = require('../models/user')
 
-module.exports.home = function (req, res) {
+module.exports.home = async function (req, res) {
 
-  // //Find all the post
-  // Post.find({},
+  try {
 
-  // });
-  // populate the whole user object(contains its name email etc) insted of userid for each post
-  Post.find({})
-    //populating the comments and users at the same time
-  .populate('user')
-  .populate({
-    // "comments" from post.js(models) file in  postSchema
-    //preloading the comments and displaying related users
-    path:'comments',
-    populate: {
-      path: 'user'
-    }
-  })
-  .exec(function (err, posts) {
+      // populate the whole user object(contains its name email etc) insted of userid for each post
+ let posts =  await Post.find({})
+ //populating the comments and users at the same time
+.populate('user')
+.populate({
+ // "comments" from post.js(models) file in  postSchema
+ //preloading the comments and displaying related users
+ path:'comments',
+ populate: {
+   path: 'user'
+ }
+})
 
-    // to show list of all the users on home page
-    // to get all the users 
-    User.find({},function(err,users){
-      return res.render('home', {
-        title: 'Codeial Home',
-        //putting the post into the context and sending it to the home view
-        posts: posts,
-        all_users: users
-      });
-    })
-    
+// to show list of all the users on home page
+ // to get all the users 
+let users = await User.find({});
 
-  });
+return res.render('home', {
+ title: 'Codeial Home',
+ //putting the post into the context and sending it to the home view
+ posts: posts,
+ all_users: users
+});
+
+  }catch (err) {
+   console.log(`Error: ${err}`)
+   return; 
+  }
+
+     
+  
 }
